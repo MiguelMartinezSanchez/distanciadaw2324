8b9b6a6213d677c41c3037940a7f4f6c0333975d
Miguel Ángel Martínez Sánchez <miguelmartinezsanchez1991@gmail.com>
2024-05-28 23:36:39 +0200

    Commit inicial para la tarea 6 de DAW

---
+++ DAW7-PHP/practica-MARTINEZ.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @file practica-MARTINEZ.php
+ * @version 1.0
+ * @author Miguel Martínez
+ * @internal Este script es un ejemplo para la generación de documentación con phpDocumentor.
+ */
+
+/**
+ * Suma dos números.
+ *
+ * @param int $a El primer número.
+ * @param int $b El segundo número.
+ * @return int La suma de los dos números.
+ */
+function sumar($a, $b) {
+    return $a + $b;
+}
+
+/**
+ * Multiplica dos números.
+ *
+ * @param int $a El primer número.
+ * @param int $b El segundo número.
+ * @return int El producto de los dos números.
+ */
+function multiplicar($a, $b) {
+    return $a * $b;
+}
+?>
+
